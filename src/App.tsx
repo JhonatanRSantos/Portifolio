880e8b9860e901ad7758ee92a73278a3486789b5
Jhonatan Rodrigues <jhonatanatm@gmail.com>
2020-08-02 07:22:49 -0300

    Created personal home page
    - Started my personal home page using reactjs

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Jhonatan R. Santos
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/JhonatanRSantos"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
